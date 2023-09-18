aef37184320c8483e14fc93fce2465b82e9e3f69
Hiuhiu02 <hp140602@gmail.com>
2023-09-18 22:40:21 +0700

    init project

---
+++ app/src/main/java/com/pmh/androidgame/GamePanel.java
@@ -0,0 +1,11 @@
+package com.pmh.androidgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GamePanel extends SurfaceView {
+
+    public GamePanel(Context context) {
+        super(context);
+    }
+}
